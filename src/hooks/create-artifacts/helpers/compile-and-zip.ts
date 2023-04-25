9afce186262eaf414c53c9e9066704e3f7cdc296
Yong Xue <eyx@g.com>
2023-04-25 19:25:41 +0800

    fix: allow handler to compile js, tsx, jsx as well

--- src/hooks/create-artifacts/helpers/compile-and-zip.ts
+++ src/hooks/create-artifacts/helpers/compile-and-zip.ts
@@ -1,16 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-require-imports */
-import { getRootPath } from "@techmmunity/utils";
 import * as Ncc from "@vercel/ncc";
-import { FunctionDefinitionHandler } from "serverless";
-import { Context } from "types/context";
+import type { FunctionDefinitionHandler } from "serverless";
+import { existsSync } from "fs";
 
+import { getRootPath } from "utils/get-root-path";
+import { Context } from "types/context";
 import { writeZip } from "utils/zip/write";
 
 interface CompileAndZipParams {
-	context: Context;
-	funcName: string;
-	serverlessFolderPath: string;
+  context: Context;
+  funcName: string;
+  serverlessFolderPath: string;
 }
 
 /**
@@ -20,77 +21,81 @@ interface CompileAndZipParams {
  */
 
 const getExternalModules = (context: Context) => {
-	const externals = context.opt?.dependenciesToExclude || [];
+  const externals = context.opt?.dependenciesToExclude || [];
 
-	if (context.opt?.excludeDependencies) {
-		const packageJson = require(getRootPath("package.json"));
+  if (context.opt?.excludeDependencies) {
+    const packageJson = require(getRootPath("package.json"));
 
-		return [
-			...externals,
-			...Object.keys(packageJson.dependencies),
-			...Object.keys(packageJson.devDependencies),
-		];
-	}
+    return [
+      ...externals,
+      ...Object.keys(packageJson.dependencies),
+      ...Object.keys(packageJson.devDependencies),
+    ];
+  }
 
-	return externals;
+  return externals;
 };
-
-/**
- *
- *
- *
- */
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getHandlerPath = (handler: any) => {
-	// Check if handler is a well-formed path based handler.
-	const handlerEntries = /(.*)\..*?$/.exec(handler);
+  // Check if handler is a well-formed path based handler.
+  const handlerEntries = /(.*)\..*?$/.exec(handler);
 
-	// Get the file path
-	const [, handlerEntry] = handlerEntries!;
+  // Get the file path
+  const [, handlerEntry] = handlerEntries!;
+  const handlerPath = findValidHandlerPath(handlerEntry);
+  if (!handlerPath) {
+    throw new Error(`Could not find handler: ${handlerEntry}`);
+  }
 
-	return {
-		rootPath: getRootPath(`${handlerEntry}.ts`),
-		path: handlerEntry,
-	};
+  return {
+    rootPath: handlerPath,
+    path: handlerEntry,
+  };
 };
 
-/**
- *
- *
- *
- */
+const ALLOWED_HANDLER_EXTENSIONS = ["ts", "js", "tsx", "jsx"];
+
+const findValidHandlerPath = (pathName: string): string | null => {
+  for (const extension of ALLOWED_HANDLER_EXTENSIONS) {
+    const handlerPath = getRootPath(`${pathName}.${extension}`);
+    if (existsSync(handlerPath)) {
+      return handlerPath;
+    }
+  }
+  return null;
+};
 
 export const compileAndZip = ({
-	funcName,
-	context,
-	serverlessFolderPath,
+  funcName,
+  context,
+  serverlessFolderPath,
 }: CompileAndZipParams) => {
-	const externals = getExternalModules(context);
+  const externals = getExternalModules(context);
 
-	const func = context.serverless.service.functions[
-		funcName
-	] as FunctionDefinitionHandler;
+  const func = context.serverless.service.functions[
+    funcName
+  ] as FunctionDefinitionHandler;
 
-	const { rootPath, path } = getHandlerPath(func.handler);
+  const { rootPath, path } = getHandlerPath(func.handler);
 
-	return Ncc(rootPath, {
-		externals,
-		quiet: true,
-		minify: context.opt?.minify,
-		sourceMap: context.opt?.sourceMap,
-		sourceMapRegister: context.opt?.sourceMapRegister,
-	}).then(({ code }) =>
-		writeZip({
-			filePath: path,
-			zipName: funcName,
-			content: code,
-			outputPath: serverlessFolderPath,
-		}).then(() => {
-			// Only sets the new values if successfully writes the zip file
-			func.package = {
-				artifact: `${serverlessFolderPath}/${funcName}.zip`,
-			};
-		}),
-	);
+  return Ncc(rootPath, {
+    externals,
+    quiet: true,
+    minify: context.opt?.minify,
+    sourceMap: context.opt?.sourceMap,
+    sourceMapRegister: context.opt?.sourceMapRegister,
+  }).then(({ code }) =>
+    writeZip({
+      filePath: path,
+      zipName: funcName,
+      content: code,
+      outputPath: serverlessFolderPath,
+    }).then(() => {
+      // Only sets the new values if successfully writes the zip file
+      func.package = {
+        artifact: `${serverlessFolderPath}/${funcName}.zip`,
+      };
+    })
+  );
 };
