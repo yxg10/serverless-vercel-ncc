8b58fe7e38a9d0cab9d86629ad098aac3bee1412
Yong Xue <eyx@g.com>
2023-04-25 19:19:07 +0800

    style: format jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,24 +2,24 @@
 /* eslint-disable capitalized-comments */
 
 module.exports = {
-	moduleFileExtensions: ["js", "json", "ts"],
-	rootDir: "src",
-	testRegex: ".*\\.spec\\.ts$",
-	transform: {
-		"^.+\\.(t|j)s$": "ts-jest",
-	},
-	collectCoverageFrom: ["**/*.ts"],
-	// setupFiles: ["./v1/tests/setup.ts"],
-	coverageDirectory: "../coverage",
-	testEnvironment: "node",
-	moduleDirectories: ["node_modules", "src"],
-	resetMocks: true,
-	// coverageThreshold: {
-	// 	global: {
-	// 		branches: 100,
-	// 		functions: 100,
-	// 		lines: 100,
-	// 		statements: 100,
-	// 	},
-	// },
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+	 	"^.+\\.(t|j)s$": "ts-jest",
+  },
+  collectCoverageFrom: ["**/*.ts"],
+  // setupFiles: ["./v1/tests/setup.ts"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  moduleDirectories: ["node_modules", "src"],
+  resetMocks: true,
+  // coverageThreshold: {
+  // 	global: {
+  // 		branches: 100,
+  // 		functions: 100,
+  // 		lines: 100,
+  // 		statements: 100,
+  // 	},
+  // },
 };
